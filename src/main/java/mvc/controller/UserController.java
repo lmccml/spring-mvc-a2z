8ccdf3f169a68a719a32a7f352f675b3b3146537
lmc <347021161@qq.com>
2019-09-15 21:43:38 +0800

    做了一丁点笔记

--- src/main/java/mvc/controller/UserController.java
+++ src/main/java/mvc/controller/UserController.java
@@ -1,6 +1,7 @@
 package mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.SpringServletContainerInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,9 @@ public class UserController {
     @RequestMapping("/getUserInfo")
     @ResponseBody
     public String getUserInfo(HttpServletRequest request) {
+        //servlet容器启动的时候会加载org.springframework.web.SpringServletContainerInitializer这个类，调用初始化类的onStartup方法
+        SpringServletContainerInitializer springServletContainerInitializer = new SpringServletContainerInitializer();
+       //进入到DispatcherServlet初始化
         return "test";
     }
 }
